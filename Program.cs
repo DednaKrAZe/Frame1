a3e5c44f93a4e9fbb775e83201044529c4b064d9
1Ckuf <ilyakorgin@gmail.com>
2025-09-26 17:35:07 +0300

    create Main

--- Program.cs
+++ Program.cs
@@ -1,8 +1,52 @@
-namespace Engineering;
+using Engineering.Context;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
+namespace Engineering;
 class Program
 {
     static void Main(string[] args)
     {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: "allow_cors",
+                policy  =>
+                {
+                    policy.WithOrigins(
+                            "http://localhost:3001",
+                            "http://localhost:3000", 
+                            "http://localhost")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+        });
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<ApplicationContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        
+        builder.Services.AddControllers();
+        
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Auth";
+            });
+
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseCors("allow_cors");
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 }
